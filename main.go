294a99fd737824ef0c3ca9876b1a765a42892f68
HBalija <balija.hrvoje@gmail.com>
2020-03-18 20:09:53 +0100

    Add udate and remove handlers

--- main.go
+++ main.go
@@ -35,9 +35,9 @@ func main() {
 	// routes with handlers
 	r.HandleFunc("/bookmarks/", getBookmarks).Methods("GET")
 	r.HandleFunc("/bookmarks/", addBookmark).Methods("POST")
+	r.HandleFunc("/bookmarks/", updateBookmark).Methods("PUT")
 	r.HandleFunc("/bookmarks/{id}/", getBookmark).Methods("GET")
-	r.HandleFunc("/bookmarks/{id}/", updateBookmark).Methods("PUT")
-	r.HandleFunc("/bookmarks/{id}/", deleteBookmark).Methods("DELETE")
+	r.HandleFunc("/bookmarks/{id}/", removeBookmark).Methods("DELETE")
 
 	// run server on port 8000 and log error if any
 	log.Fatal(http.ListenAndServe(":8000", r))
@@ -53,10 +53,9 @@ func addBookmark(w http.ResponseWriter, r *http.Request) {
 	var b bookmark
 	// decode pointer response body into "b" memory address
 	json.NewDecoder(r.Body).Decode(&b)
-
 	bs = append(bs, b)
 
-	// return response (201)
+	// return status 201
 	json.NewEncoder(w).Encode(http.StatusCreated)
 }
 
@@ -68,14 +67,35 @@ func getBookmark(w http.ResponseWriter, r *http.Request) {
 		if v.ID == id {
 			// return response
 			json.NewEncoder(w).Encode(v)
+			break
 		}
 	}
 }
 
-func updateBookmark(http.ResponseWriter, *http.Request) {
-	log.Println("Update a bookmark")
+func updateBookmark(w http.ResponseWriter, r *http.Request) {
+	var b bookmark
+	json.NewDecoder(r.Body).Decode(&b)
+
+	for i, v := range bs {
+		if v.ID == b.ID {
+			bs[i] = b
+			// return status 200
+			json.NewEncoder(w).Encode(http.StatusOK)
+			break
+		}
+	}
 }
 
-func deleteBookmark(http.ResponseWriter, *http.Request) {
-	log.Println("Delete a bookmark")
+func removeBookmark(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for i, v := range bs {
+		if v.ID == id {
+			bs = append(bs[:i], bs[(i+1):]...) // slice till "i" and add values from "i" to end
+			break
+		}
+	}
+	// return status 204
+	json.NewEncoder(w).Encode(http.StatusNoContent)
 }
